232242e20612b856c9cbc0d9c7f3ee43dcad17b1
Angel Lucero <alucero-2021211@kinal.edu.gt>
2026-02-21 18:39:14 -0600

    feat: se agregao configuracion de los campos del estudiante

---
+++ src/fields/field.model.js
@@ -0,0 +1,53 @@
+'use strict';
+
+import mongoose from "mongoose";
+
+const fieldSchema = mongoose.Schema(
+    {
+        fieldName: {
+            type: String,
+            required: [true, 'El nombre del estudiante es requerido'],
+            trim: true,
+            maxLength: [100, 'El nombre no puede exceder 100 caracteres']
+        },
+        fieldSurname: {
+            type: String,
+            required: [true, 'El apellido del estudiante es requerido'],
+            trim: true,
+            maxLength: [100, 'El apellido no puede exceder 100 caracteres']
+        },
+        idCard: {
+            type: String,
+            required: [true, 'El carnet de identidad es requerido'],
+            trim: true,
+            unique: true,
+            maxLength: [20, 'El carnet no puede exceder 20 caracteres']
+        },
+        grade: {
+            type: String,
+            required: [true, 'El grado del estudiante es requerido'],
+            enum: {
+                values: ['1RO', '2DO', '3RO', '4TO', '5TO', '6TO'],
+                message: 'Grado no válido'
+            }
+        },
+        photo: {
+            type: String,
+            default: null,
+        },
+        isActive: {
+            type: Boolean,
+            default: true
+        }
+    },
+    {
+        timestamps: true,
+        versionKey: false
+    }
+)
+
+fieldSchema.index({ isActive: 1});
+fieldSchema.index({ grade: 1});
+fieldSchema.index({ isActive: 1, grade: 1});
+
+export default mongoose.model('Field', fieldSchema);
